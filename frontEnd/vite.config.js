071b461d26093f2ee63fc31dc087417e0ad5fa7d
Dazzle <catc.daz07@gmail.com>
2025-01-10 19:04:53 +0800

    upate 7:04

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Store-System/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
