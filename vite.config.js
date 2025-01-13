477c170ef4e1a182c0d309e2f5a5b8ed7fa18778
Dazzle <catc.daz07@gmail.com>
2025-01-13 18:43:22 +0800

    ds

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Store-System/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/src/main.jsx',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-});
+  base:"/Store-System"
+})
